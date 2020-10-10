17c0dd1080e370e8e3ecaabf4e868851ef05538c
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-10-09 23:51:34 -0400

    complete python mvp

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -4,12 +4,26 @@
 
 a)
 
+```
+The loops make this a O(n)
+```
 
 b)
 
+```
+Because of the inclusion of the nested loops, it makes it O(n^2)
+```
 
 c)
+```
+This is O(n).
+```
 
 ## Exercise II
 
+```python
 
+'''
+
+'''
+```
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,16 @@
 '''
 Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should return a count of how many occurrences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
+
     # TBC
-    
-    pass
+    # check if the first two indexes contains 'th', if not, call word, minus the first index
+    if len(word) == 0: #Base Case
+        return 0
+
+    if word[0:2] == 'th':
+        return count_th(word[1:]) + 1
+    else:
+        return count_th(word[1:])
